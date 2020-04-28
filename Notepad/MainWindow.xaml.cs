d1d2c6f698bc1e498ee6c609f248e6359f34ff8c
tori690 <tori690@yandex.ru>
2020-04-28 19:55:51 +0300

    Оптимизация счетчика выделенных символов.

--- Notepad/MainWindow.xaml.cs
+++ Notepad/MainWindow.xaml.cs
@@ -379,8 +379,11 @@ namespace Notepad
             int lineIndex = tbNotepad.GetLineIndexFromCharacterIndex(tbNotepad.SelectionStart);
             int colIndex = tbNotepad.SelectionStart - tbNotepad.GetCharacterIndexFromLineIndex(lineIndex);
             sbiCursorPosition.Content = $"Стр {lineIndex + 1}, стлб {colIndex + 1}";
-            sbiTextSelectLenGet.Content = $"({tbNotepad.SelectedText.Length})";
-            sbiTextSelectLenWithoutSpacesGet.Content = $"({(((tbNotepad.SelectedText.Replace('\n', ' ')).Replace('\r', ' ')).Replace(" ", "")).Length})";
+            if (tbNotepad.SelectionLength > 0)
+            {
+                sbiTextSelectLenGet.Content = $"({tbNotepad.SelectionLength})";
+                sbiTextSelectLenWithoutSpacesGet.Content = $"({(((tbNotepad.SelectedText.Replace('\n', ' ')).Replace('\r', ' ')).Replace(" ", "")).Length})";
+            }
         }
 
         private void mmHelpAboutProgram_Click(object sender, RoutedEventArgs e)
